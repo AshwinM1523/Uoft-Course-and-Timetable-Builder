14b5e04891c02a2df97888b69a43fe34f58dbf00
AshwinM1523 <ashwinmallik@hotmail.com>
2022-11-25 14:30:38 -0500

    signup confirmation page functionality

--- CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/ConfirmSignupActivity.java
+++ CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/ConfirmSignupActivity.java
@@ -2,13 +2,30 @@ package com.example.courseplannerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConfirmSignupActivity extends AppCompatActivity {
 
+    private Button signUpConfirm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_signup);
+
+        signUpConfirm = (Button) findViewById(R.id.signup_confirmation_login_btn);
+        signUpConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHomePage();
+            }
+        });
+    }
+
+    public void openHomePage(){
+        Intent intent = new Intent(this, HomepageActivity.class);
+        startActivity(intent);
     }
 }
